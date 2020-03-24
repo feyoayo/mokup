eecd496b3e13fc935be1c486d8dff1ebfc24c55b
Andrey Nemirov <nemirov.andrei@gmail.com>
2020-03-24 02:25:06 +0200

    finish woking on team section

---
+++ makettry/index.php
@@ -0,0 +1,2 @@
+<?php
+echo Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat, possimus praesentium voluptates, ipsum deleniti ut ratione esse quod laborum assumenda amet laboriosam magnam porro nam officiis quidem. Ipsam, rem dolorem?;
