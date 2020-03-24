2e845ed8a7f33d818e02f8d66b964f3261db236c
feyoayo <nemirov.andrei@gmail.com>
2020-03-24 22:12:21 +0200

    Cuda project

--- makettry/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-echo Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat, possimus praesentium voluptates, ipsum deleniti ut ratione esse quod laborum assumenda amet laboriosam magnam porro nam officiis quidem. Ipsam, rem dolorem?;
